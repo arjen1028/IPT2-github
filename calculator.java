d2aa9f9d1a1a9328c9b5e64425a9a37f326f46b1
arjen <am.simbillo28@gmail.com>
2024-09-10 20:40:33 +0800

    “5th”

--- calculator.java
+++ calculator.java
@@ -1,4 +1,9 @@
 public class calculator{
- public static int multiply(int a, int b){
- return a*b;
- }
+public static int divide(int a, int b){
+if(b == 0){
+throw new
+ArithmeticException("Cannot divided by
+zero.");
+}
+return a/b;
+}
